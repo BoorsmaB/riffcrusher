6961fd342fa31e4f1c2faeef9cbe6332e981365e
Bryan Boorsma <bboorsma90@hotmail.com>
2025-06-17 10:42:50 +0200

    updated navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,26 +5,28 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <header>
-      <div className="logo">
-        <img src="/assets/riffcrusherlogo.png" alt="Logo" />
-        <p className="slogan">"We love everything loud!"</p>
+      <div className="header-content">
+        <div className="logo">
+          <img src="/assets/riffcrusherlogo.png" alt="Logo" />
+          <p className="slogan">"We love everything loud!"</p>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/reviews">Reviews</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
       </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/reviews">Reviews</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
     </header>
   );
 }
